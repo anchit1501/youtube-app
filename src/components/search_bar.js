19562f5cd516298e8b26d80cbb3f635dcf13f628
Anchit Aggarwal <1501.anchit@gmail.com>
2018-06-26 06:38:05 +0530

    Video List and Video Player Added

---
+++ src/components/search_bar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+    render() {
+        return (<div><input 
+            value={this.state.term}/*we are making input field a controlled component*/
+            /*this.setState causes the value to rerender and whenever the value is rerendered it will put up the new value in searchbar */
+            /*This is similar to outube search ie when you search a  video the new keyword is placed in the searchbar */
+            onChange={event => this.setState({ term: event.target.value })/*State Updation, onChange updates the state*/
+        }
+        />
+    </div>)
+    }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDom,{render} from 'react-dom';
+import Searchbar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail';
 
-const API_KEY='AIzaSyCUferbQuY6fpLpG07OHmqaycL6orcY58s';
+const API_KEY = 'AIzaSyCUferbQuY6fpLpG07OHmqaycL6orcY58s';
 
-const App=()=>{
-    return <div>Hello</div>;
-}
+class App extends Component {
 
-ReactDom.render(<App />,document.querySelector('.container'));
+    constructor(props){
+        super(props);
+
+        this.state={videos:[]};
+
+        YTSearch({key:API_KEY,term:'batman'},(videos)=>{
+            this.setState({videos:videos});/*in an object whenever the key and value are identical terms  we can refactor from this.setState({videos:videos}) to the implied one*/
+        });/*putting serach function in cosntructor will call the search api instantly whenever the instance of index.js is created */
+    }
+        render(){
+            return (
+                < div > 
+                < Searchbar />
+                <VideoDetail video={this.state.videos[0]}/>
+                <VideoList videos={this.state.videos} /> 
+                 </div>);
+            }
+        }
+
+        ReactDom.render( <App /> , document.querySelector('.container'));
+        
+        
+        
+        
+        /*passing data from parent to data is called props */
+          /*downwards data flow*/
