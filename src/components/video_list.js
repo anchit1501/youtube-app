1e7c37a38541c8f8cfc7ff431d664d727b4a8402
Anchit Aggarwal <1501.anchit@gmail.com>
2018-06-26 06:56:08 +0530

    Video Selection Implimented

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,10 @@ import VideoListItem from './video_list_item';
 
 const VideoList=(props)=>{
 const videoItems = props.videos.map((video)=>{
-        return<VideoListItem key={video.etag} video={video} />
+        return<VideoListItem 
+        onVideoSelect={props.onVideoSelect}/*passing callback to videi_list_item */
+        key={video.etag} 
+        video={video} />
     });
     return(
         <ul className="col-md-4 list-group">
--- src/index.js
+++ src/index.js
@@ -11,19 +11,24 @@ class App extends Component {
 
     constructor(props){
         super(props);
+        
+        this.state={videos:[],
+        selectedVideo:null};/*setting 1st video as default video */
 
-        this.state={videos:[]};
 
         YTSearch({key:API_KEY,term:'batman'},(videos)=>{
-            this.setState({videos:videos});/*in an object whenever the key and value are identical terms  we can refactor from this.setState({videos:videos}) to the implied one*/
+            this.setState({videos:videos,
+            selectedVideo:videos[0]});/*in an object whenever the key and value are identical terms  we can refactor from this.setState({videos:videos}) to the implied one*/
         });/*putting serach function in cosntructor will call the search api instantly whenever the instance of index.js is created */
     }
         render(){
             return (
-                < div > 
-                < Searchbar />
-                <VideoDetail video={this.state.videos[0]}/>
-                <VideoList videos={this.state.videos} /> 
+                <div> 
+                <Searchbar />
+                <VideoDetail video={this.state.selectedVideo}/>{/*passing data from parent to data is called props */}
+                <VideoList 
+                onVideoSelect={selectedVideo=>this.setState({selectedVideo})}
+                videos={this.state.videos} /> {/*updates app state to selected video, we pass this as property to video list and which further passes it to video list items and callbacks the function on click*/}
                  </div>);
             }
         }
@@ -33,5 +38,4 @@ class App extends Component {
         
         
         
-        /*passing data from parent to data is called props */
           /*downwards data flow*/
