f2c7bcb9e9e064f3cde1e4e35f85ddf2b34d363a
Anchit Aggarwal <1501.anchit@gmail.com>
2018-06-28 15:53:34 +0530

    Throttling Search Term

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,15 +8,20 @@ class SearchBar extends Component {
         this.state = { term: '' };
     }
     render() {
-        return (<div><input 
+        return (<div className="search-bar"><input 
             value={this.state.term}/*we are making input field a controlled component*/
             /*this.setState causes the value to rerender and whenever the value is rerendered it will put up the new value in searchbar */
             /*This is similar to outube search ie when you search a  video the new keyword is placed in the searchbar */
-            onChange={event => this.setState({ term: event.target.value })/*State Updation, onChange updates the state*/
+            onChange={event => this.onInputChange(event.target.value)/*State Updation, onChange updates the state*/
         }
         />
     </div>)
     }
+onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+}
+
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Searchbar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list'
 import VideoDetail from './components/video_detail';
-
+import _ from 'lodash';
 const API_KEY = 'AIzaSyCUferbQuY6fpLpG07OHmqaycL6orcY58s';
 
 class App extends Component {
@@ -15,16 +15,23 @@ class App extends Component {
         this.state={videos:[],
         selectedVideo:null};/*setting 1st video as default video */
 
+            this.videoSearch('batman');
 
-        YTSearch({key:API_KEY,term:'batman'},(videos)=>{
+    }
+
+    videoSearch(term){
+        
+        YTSearch({key:API_KEY,term:term},(videos)=>{
             this.setState({videos:videos,
             selectedVideo:videos[0]});/*in an object whenever the key and value are identical terms  we can refactor from this.setState({videos:videos}) to the implied one*/
         });/*putting serach function in cosntructor will call the search api instantly whenever the instance of index.js is created */
+
     }
         render(){
+            const videoSearch = _.debounce((term)=>{this.videoSearch(term)},3000);
             return (
                 <div> 
-                <Searchbar />
+                <Searchbar  onSearchTermChange={term=>this.videoSearch(term)} />
                 <VideoDetail video={this.state.selectedVideo}/>{/*passing data from parent to data is called props */}
                 <VideoList 
                 onVideoSelect={selectedVideo=>this.setState({selectedVideo})}
